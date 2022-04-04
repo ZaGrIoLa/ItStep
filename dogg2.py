2e6ae1cff3dfcb2c4588881a1a715104f971d2a9
User <zagriola@gmail.com>
2022-04-04 19:45:43 +0300

    first commit

--- dogg2.py
+++ dogg2.py
@@ -5,23 +5,46 @@ class Dog:
         self.sleeptime=50
         self.satiety=0
         self.lovetime=0
+        self.play=0
+        self.training=0
+        self.mind=0
         self.alive=True
     def to_sleep(self):
         print("Time to sleep!")
         self.sleeptime+=3
         self.lovetime+=0.12
         self.satiety-=0.10
+        self.mind+=0.03
+        self.training-=0.03
     def to_eat(self):
         print("Time to eat!")
         self.satiety+=3
         self.sleeptime-=0.3
         self.lovetime-=0.1
+        self.mind+=0.3
+        self.training+=0.03
     def to_love(self):
         print("Love time")
         self.lovetime+=3
-
+        self.play+=1
         self.satiety-=0.1
         self.sleeptime-=0.1
+        self.mind+=0.01
+    def to_training(self):
+        print("Time to trainyng!")
+        self.training+= 3
+        self.mind+=0.5
+        self.play+=0.3
+        self.sleeptime-=0.2
+        self.satiety-=0.5
+    def to_play(self):
+        print("Time to play!")
+        self.play+=3
+        self.training+=0.3
+        self.sleeptime-=0.3
+        self.lovetime-=0.3
+        self.satiety-=0.3
+        self.mind+=0.5
     def is_alive(self):
         if self.sleeptime < -0.5:
             print("Died...")
@@ -32,10 +55,19 @@ class Dog:
         elif self.satiety < -0.5:
             print("Died too...")
             self.alive=False
+        elif self.mind > 1:
+            print("Stupid...")
+            self.alive=True
+        elif self.play > 1:
+            print("Sadly..")
+            self.alive=True
     def end_of_day(self):
       print(f"Satiety={self.satiety}")
       print(f"Sleeptime={self.sleeptime,2}")
       print(f"Lovetime={self.lovetime}")
+      print(f"Play={self.play}")
+      print(f"Training={self.training}")
+      print(f"Mind={self.mind}")
     def live(self, day):
       day="Day" + str(day) + "of" + self.name + "file"
       print(f"{day:=^50}")
@@ -48,8 +80,8 @@ class Dog:
         self.to_love()
       self.end_of_day()
       self.is_alive()
-milly= Dog(name="Milly")
+fancy= Dog(name="Fancy")
 for day in range(365):
-  if milly.alive==False:
+  if fancy.alive==False:
     break
-  milly.live(day)
+  fancy.live(day)
